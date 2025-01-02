c20df7a0e06559c10279c3a55e9570a3da414d66
Leon <tahjib.leon399@gmail.com>
2025-01-02 23:36:44 +0600

    auth page redesign

--- workflowx-client/src/app/authProvider.tsx
+++ workflowx-client/src/app/authProvider.tsx
@@ -24,7 +24,7 @@ const components = {
                     alt="Amplify logo"
                     src="https://docs.amplify.aws/assets/logo-dark.svg"
                     className="flex justify-center mt-10"
-                />
+                />q
             </View>
         );
     },
@@ -39,148 +39,6 @@ const components = {
                 </Text>
             </View>
         );
-    },
-
-    SignIn: {
-        Header() {
-            const { tokens } = useTheme();
-
-            return (
-                <Heading
-                    padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
-                    level={3}
-                >
-                    Sign in to your account
-                </Heading>
-            );
-        },
-        Footer() {
-            const { toForgotPassword } = useAuthenticator();
-
-            return (
-                <View textAlign="center">
-                    <Button
-                        fontWeight="normal"
-                        onClick={toForgotPassword}
-                        size="small"
-                        variation="link"
-                    >
-                        Reset Password
-                    </Button>
-                </View>
-            );
-        },
-    },
-
-    SignUp: {
-        Header() {
-            const { tokens } = useTheme();
-
-            return (
-                <Heading
-                    padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
-                    level={3}
-                >
-                    Create a new account
-                </Heading>
-            );
-        },
-        Footer() {
-            const { toSignIn } = useAuthenticator();
-
-            return (
-                <View textAlign="center">
-                    <Button
-                        fontWeight="normal"
-                        onClick={toSignIn}
-                        size="small"
-                        variation="link"
-                    >
-                        Back to Sign In
-                    </Button>
-                </View>
-            );
-        },
-    },
-    ConfirmSignUp: {
-        Header() {
-            const { tokens } = useTheme();
-            return (
-                <Heading
-                    padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
-                    level={3}
-                >
-                    Enter Information:
-                </Heading>
-            );
-        },
-        Footer() {
-            return <Text>Footer Information</Text>;
-        },
-    },
-    SetupTotp: {
-        Header() {
-            const { tokens } = useTheme();
-            return (
-                <Heading
-                    padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
-                    level={3}
-                >
-                    Enter Information:
-                </Heading>
-            );
-        },
-        Footer() {
-            return <Text>Footer Information</Text>;
-        },
-    },
-    ConfirmSignIn: {
-        Header() {
-            const { tokens } = useTheme();
-            return (
-                <Heading
-                    padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
-                    level={3}
-                >
-                    Enter Information:
-                </Heading>
-            );
-        },
-        Footer() {
-            return <Text>Footer Information</Text>;
-        },
-    },
-    ForgotPassword: {
-        Header() {
-            const { tokens } = useTheme();
-            return (
-                <Heading
-                    padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
-                    level={3}
-                >
-                    Enter Information:
-                </Heading>
-            );
-        },
-        Footer() {
-            return <Text>Footer Information</Text>;
-        },
-    },
-    ConfirmResetPassword: {
-        Header() {
-            const { tokens } = useTheme();
-            return (
-                <Heading
-                    padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
-                    level={3}
-                >
-                    Enter Information:
-                </Heading>
-            );
-        },
-        Footer() {
-            return <Text>Footer Information</Text>;
-        },
     },
 }
 
@@ -221,7 +79,7 @@ const AuthProvider = ({ children }: any) => {
                     user ? (
                         <div>{children}</div>
                     ) : (
-                        <div className="flex justify-center items-center h-screen bg-slate-600">
+                        <div>
                             <h1>Please sign in below:</h1>
                         </div>
                     )
